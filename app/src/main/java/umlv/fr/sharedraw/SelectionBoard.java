e199cf48bcd502686bec8366bc87ea98badc3b40
ggalland <galland.gaetan@gmail.com>
2016-04-19 16:00:22 +0200

    images ok !

--- app/src/main/java/umlv/fr/sharedraw/SelectionBoard.java
+++ app/src/main/java/umlv/fr/sharedraw/SelectionBoard.java
@@ -1,9 +1,12 @@
 package umlv.fr.sharedraw;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -24,38 +27,15 @@ public class SelectionBoard extends AppCompatActivity implements AsyncTaskRespon
 
         ListView listViewBoard = (ListView) findViewById(R.id.listView_board);
 
-        String[] items = { "Board1", "Board2", "Board3", "Board4", "Board5","Board6", "Board7", "Board8", "Board9", "Board10","Board11","Board12", "Board13", "Board14", "Board15", "Board16" };
-
-        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, items);
-
-        listViewBoard.setAdapter(adapter);*/
-
         //Création de la ArrayList qui nous permettra de remplir la listView
         ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
 
-        //On déclare la HashMap qui contiendra les informations pour un item
-        HashMap<String, String> map;
-
         //On refait la manip plusieurs fois avec des données différentes pour former les items de notre ListView
+        addNewBoard(listItem,"A Titre Board 1","Description");
 
-        map = new HashMap<String, String>();
-        map.put("titre", "Excel");
-        map.put("description", "Tableur");
-        map.put("img", String.valueOf(R.drawable.a));
-        listItem.add(map);
+        addNewBoard(listItem,"C Titre Board 2","Description");
 
-        map = new HashMap<String, String>();
-        map.put("titre", "Power Point");
-        map.put("description", "Logiciel de présentation");
-        map.put("img", String.valueOf(R.drawable.b));
-        listItem.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("titre", "Outlook");
-        map.put("description", "Client de courrier électronique");
-        map.put("img", String.valueOf(R.drawable.c));
-        listItem.add(map);
+        addNewBoard(listItem,"Titre Board 3","Description");
 
         //Création d'un SimpleAdapter qui se chargera de mettre les items présents dans notre list (listItem) dans la vue affichageitem
         SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.affichage_item_list_view,
@@ -64,11 +44,29 @@ public class SelectionBoard extends AppCompatActivity implements AsyncTaskRespon
         //On attribue à notre listView l'adapter que l'on vient de créer
         listViewBoard.setAdapter(mSchedule);
 
+        //Enfin on met un écouteur d'évènement sur notre listView
+        listViewBoard.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+
+            }
+        });
 
         /* Test AsyncTask
         HttpRequest httpRequest = HttpRequest.createHttpRequest();
         httpRequest.setDelegate(this);
         httpRequest.execute("getListOfDashboard", "127.0.0.1:12345"); */
+    }
+
+    private void addNewBoard(ArrayList listItem,String titre,String description){
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("titre", titre);
+        map.put("description", description);
+        String nameOfImage = titre.substring(0,1);
+        nameOfImage =  nameOfImage.toLowerCase();
+        map.put("img", String.valueOf(getResources().getIdentifier(nameOfImage, "drawable", this.getPackageName())));
+        listItem.add(map);
     }
 
     @Override
@@ -87,11 +85,16 @@ public class SelectionBoard extends AppCompatActivity implements AsyncTaskRespon
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add:
+
+
                 Toast.makeText(getApplicationContext(),"Add new board... Please wait.",Toast.LENGTH_SHORT).show();
                 // TODO
+
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
 }
