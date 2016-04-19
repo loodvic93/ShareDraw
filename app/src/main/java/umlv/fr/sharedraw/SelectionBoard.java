efe3a3902d2111eae518fbdd8acbc410e327204d
ggalland <galland.gaetan@gmail.com>
2016-04-19 11:56:56 +0200

    menu

---
+++ app/src/main/java/umlv/fr/sharedraw/SelectionBoard.java
@@ -0,0 +1,13 @@
+package umlv.fr.sharedraw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectionBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_board);
+    }
+}
