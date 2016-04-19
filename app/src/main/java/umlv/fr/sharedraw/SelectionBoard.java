186c6cf9767b87d4cfe06e21a7c7a80730e59faf
ggalland <galland.gaetan@gmail.com>
2016-04-19 14:19:27 +0200

    menu

--- app/src/main/java/umlv/fr/sharedraw/SelectionBoard.java
+++ app/src/main/java/umlv/fr/sharedraw/SelectionBoard.java
@@ -6,7 +6,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SelectionBoard extends AppCompatActivity {
 
@@ -19,10 +23,43 @@ public class SelectionBoard extends AppCompatActivity {
 
         String[] items = { "Board1", "Board2", "Board3", "Board4", "Board5","Board6", "Board7", "Board8", "Board9", "Board10","Board11","Board12", "Board13", "Board14", "Board15", "Board16" };
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, items);
 
-        listViewBoard.setAdapter(adapter);
+        listViewBoard.setAdapter(adapter);*/
+
+        //Création de la ArrayList qui nous permettra de remplir la listView
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+
+        //On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> map;
+
+        //On refait la manip plusieurs fois avec des données différentes pour former les items de notre ListView
+
+        map = new HashMap<String, String>();
+        map.put("titre", "Excel");
+        map.put("description", "Tableur");
+        map.put("img", String.valueOf(R.drawable.a));
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("titre", "Power Point");
+        map.put("description", "Logiciel de présentation");
+        map.put("img", String.valueOf(R.drawable.b));
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("titre", "Outlook");
+        map.put("description", "Client de courrier électronique");
+        map.put("img", String.valueOf(R.drawable.c));
+        listItem.add(map);
+
+        //Création d'un SimpleAdapter qui se chargera de mettre les items présents dans notre list (listItem) dans la vue affichageitem
+        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.affichage_item_list_view,
+                new String[] {"img", "titre", "description"}, new int[] {R.id.img, R.id.titre, R.id.description});
+
+        //On attribue à notre listView l'adapter que l'on vient de créer
+        listViewBoard.setAdapter(mSchedule);
     }
 
     @Override
