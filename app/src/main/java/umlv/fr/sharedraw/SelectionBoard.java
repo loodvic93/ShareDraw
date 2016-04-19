f9b4dafdd95596679615b80738250b7e56d6dc01
loodvic93 <ludovic.carretti@gmail.com>
2016-04-19 12:21:04 +0200

    ALL OK

--- app/src/main/java/umlv/fr/sharedraw/SelectionBoard.java
+++ app/src/main/java/umlv/fr/sharedraw/SelectionBoard.java
@@ -2,6 +2,8 @@ package umlv.fr.sharedraw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SelectionBoard extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class SelectionBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection_board);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                // TODO
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
