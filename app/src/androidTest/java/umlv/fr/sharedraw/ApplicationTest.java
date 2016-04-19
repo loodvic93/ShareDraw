b31f8abdd44d0d23a2b79c6a808742a8bf97b3f1
loodvic93 <ludovic.carretti@gmail.com>
2016-04-19 12:00:41 +0200

    ALL OK

--- app/src/androidTest/java/umlv/fr/sharedraw/ApplicationTest.java
+++ app/src/androidTest/java/umlv/fr/sharedraw/ApplicationTest.java
@@ -3,6 +3,8 @@ package umlv.fr.sharedraw;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import org.json.JSONArray;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
